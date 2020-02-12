e0c391aa206c2eafb8bbc20e24d037802c1ed909
Kushal chapagain <53593915+Kushalchg@users.noreply.github.com>
2020-02-12 08:29:03 -0800

    Add files via upload

---
+++ rock_paper_scissors.py
@@ -0,0 +1,40 @@
+
+import random
+
+
+
+
+choice_list = ['rock','scissors','paper']
+
+computer_input = random.choice(choice_list)
+
+user_input = (input("enter your choice:  "))
+
+
+
+if (computer_input=="rock" and user_input=="scissors"):
+    print("computer choice:  "+computer_input)
+    print("you loose")
+elif(computer_input =="paper" and user_input=="rock"):
+    print("computer choice:  " + computer_input)
+    print("you loose")
+elif(computer_input=="scissors" and user_input=="paper"):
+    print("computer choice:  " + computer_input)
+    print("you loose")
+
+elif(computer_input == "rock" and user_input == "rock"):
+    print("computer choice:  " + computer_input)
+    print("you draw")
+elif (computer_input == "paper" and user_input == "paper"):
+    print("computer choice:  " + computer_input)
+    print("you draw")
+elif (computer_input == "scissors" and user_input == "scissors"):
+    print("computer choice:  " + computer_input)
+    print("you draw")
+elif(user_input !="scissors"or"paper"or"rock" ):
+    print("computer choice:  " + computer_input)
+    print("you entered wrong word please check spelling")
+
+else:
+    print("computer choice:  "+computer_input)
+    print("you win")
